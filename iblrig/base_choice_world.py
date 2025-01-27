706b8c7f56b00e1fc33c14678a8ed2314de3ddad
Florian Rau <bimac@users.noreply.github.com>
2025-01-27 17:16:34 +0100

    only compute signed_contrast for the filled portion of trials_table
    to avoid issues with the combination of `np.sign()` and `pd.NA`

--- iblrig/base_choice_world.py
+++ iblrig/base_choice_world.py
@@ -971,7 +971,8 @@ class TrainingChoiceWorldSession(ActiveChoiceWorldSession):
 
     def compute_performance(self):
         """Aggregate the trials table to compute the performance of the mouse on each contrast."""
-        self.trials_table['signed_contrast'] = self.trials_table.contrast * np.sign(self.trials_table.position)
+        idx = self.trials_table.position.notna()
+        self.trials_table.signed_contrast[idx] = self.trials_table.contrast[idx] * np.sign(self.trials_table.position[idx])
         performance = self.trials_table.groupby(['signed_contrast']).agg(
             last_50_perf=pd.NamedAgg(column='trial_correct', aggfunc=lambda x: np.sum(x[np.maximum(-50, -x.size) :]) / 50),
             ntrials=pd.NamedAgg(column='trial_correct', aggfunc='count'),
